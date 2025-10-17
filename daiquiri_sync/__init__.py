7fda144c77d89a4ea5dbc573b35f8bbd893542a7
kimakan <45099849+kimakan@users.noreply.github.com>
2025-10-17 14:53:36 +0200

    Replace setup.py with pyproject.toml

--- daiquiri_sync/__init__.py
+++ daiquiri_sync/__init__.py
@@ -1,9 +1,4 @@
-__title__ = 'django-daiquiri-sync'
-__version__ = '0.1.0'
-__author__ = 'Jochen Klar'
-__email__ = 'jklar@aip.de'
-__license__ = 'Apache-2.0'
-__copyright__ = 'Copyright 2018 Leibniz Institute for Astrophysics Potsdam (AIP)'
+__version__ = '0.2.0'
 
 from .ansible import Ansible
 from .database import Database
